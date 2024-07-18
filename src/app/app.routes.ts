cec20e4b8cf5965258f3163b349c90a6407679d6
antonioszm <antonioszm@outlook.com>
2024-07-18 16:17:36 -0300

    Exercicio 2 - Criacao de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/pages/home/home.component';
+import { AlunosComponent } from './shared/components/pages/alunos/alunos.component';
+import { LoginComponent } from './shared/components/pages/login/login.component';
+import { CadastroComponent } from './shared/components/pages/cadastro/cadastro.component';
+import { DisciplinasComponent } from './shared/components/pages/disciplinas/disciplinas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "alunos",
+        component: AlunosComponent
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "cadastro",
+        component: CadastroComponent
+    },
+    {
+        path: "disciplinas",
+        component: DisciplinasComponent
+    }
+];
