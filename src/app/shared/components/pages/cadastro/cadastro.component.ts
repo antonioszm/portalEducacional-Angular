e1cda38c324b223d52ce85a3644de02243b32517
antonioszm <antonioszm@outlook.com>
2024-09-21 14:03:00 -0300

    feat: edicao de aluno

--- src/app/shared/components/pages/cadastro/cadastro.component.ts
+++ src/app/shared/components/pages/cadastro/cadastro.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlunoService } from '../../../services/aluno.service';
+import { Validators, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+  alunoForm!: FormGroup;
+  alunoId: string | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private alunoService: AlunoService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.alunoForm = this.fb.group({
+      nomeCompleto: ['', Validators.required],
+      cpf: ['', [Validators.required, Validators.minLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      celular: ['', Validators.required],
+      senha: ['', Validators.required],
+      curso: ['', Validators.required]
+    });
+
+    this.alunoId = this.route.snapshot.paramMap.get('id');
+    if (this.alunoId) {
+      this.carregarAluno(this.alunoId);
+    }
+  }
+
+  carregarAluno(id: string): void {
+    this.alunoService.getAlunoById(id).subscribe((aluno) => {
+      this.alunoForm.patchValue({
+        nomeCompleto: aluno.nomeCompleto,
+        cpf: aluno.cpf,
+        email: aluno.email,
+        celular: aluno.celular,
+        senha: aluno.senha,
+        curso: aluno.curso
+      });
+    });
+  }
+
+  onSubmit(): void {
+    if (this.alunoForm.valid) {
+      if (this.alunoId) {
+        this.alunoService.updateAluno(this.alunoId, this.alunoForm.value).subscribe(() => {
+          alert('Aluno atualizado com sucesso!');
+        });
+      } else {
+        this.alunoService.postAluno(this.alunoForm.value).subscribe(() => {
+          alert('Aluno criado com sucesso!');
+        });
+      }
+    }
+  }
 }
--- src/app/shared/services/aluno.service.ts
+++ src/app/shared/services/aluno.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlunoService {
-  private apiUrl = 'http://localhost:3000/user'; 
+  private apiUrl = 'http://localhost:3000/users'; 
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   
   constructor(private http: HttpClient) { }
 
+  postAluno(aluno: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, aluno, this.httpOptions);
+  }
   getAlunos(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
