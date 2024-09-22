412452d8b38909d0c175b86fe34515548a17270f
antonioszm <antonioszm@outlook.com>
2024-09-22 14:28:00 -0300

    feat: header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { ToolbarModule } from 'primeng/toolbar';
 
-@Component({
+ @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [AvatarModule, MatToolbarModule, CommonModule, ToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  nomeUsuario: string | null = '';
+  showLogout = false;
 
+  constructor(private authService: AuthService){}
+
+  ngOnInit(): void {
+    this.obterNomeUsuario();
+  }
+  obterNomeUsuario(): void {
+    const usuarioLogado = localStorage.getItem('usuariologado');
+    if (usuarioLogado) {
+        const aluno = JSON.parse(usuarioLogado);
+        this.nomeUsuario = aluno.nomeCompleto;
+    }
+  }
+  toggleLogout(): void {
+    this.showLogout = !this.showLogout;
+  }
+  deslogar(){
+    this.authService.logout()
+  }
 }
